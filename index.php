9144b3e11c5eec94a3f565bc250368fd5d66728e
Alexandre SOUGAKOFF <asougako@e1r10p20.42.fr>
2018-01-29 16:24:20 +0100

    merge

--- index.php
+++ index.php
@@ -14,8 +14,8 @@ https://www.alsacreations.com/tuto/lire/1417-zoom-sur-effet-parallaxe.html
 <HTML lang="fr">
 	<HEAD>
 		<meta charset="UTF-8">
-		<link rel="icon" type="image/png" src="http://piq.codeus.net/static/media/userpics/piq_261595_400x400.png">
-		<link type="text/css" rel="stylesheet" href="style.css">
+		<link rel="icon" type="image/png" href="resources/cv.png" />
+		<link rel="stylesheet" type="text/css" href="style.css">
 		<script src="./resp.js"></script>
 		<script src="./ajax.js"></script>
 		<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js"></script>
@@ -25,89 +25,12 @@ https://www.alsacreations.com/tuto/lire/1417-zoom-sur-effet-parallaxe.html
 
 	</HEAD>
 	<BODY>
-
-
-			<a id="top" href="#bot">TO BOTTOM</a>
-		<div id="bg">
-			<div id="content">
-				<div id="author">
-					<p class="author-name">Gontrand Théogal</p>
-					<hr class="author-separator" />
-					<p class="author-job">Mage de 12ème niveau</p>
-				</div>
-				<div id="portrait">
-					<div id="rotate">
-						<img src="./resources/troll.png" title="portrait" alt="troll" />
-					</div>
-				</div>
-				<div id="div1">
-					<div id="div2">
-						<H2 onclick="myFunction2('para_info')">Infos perso</H2>
-						<p id="para_info"><?php $para='info'; include 'lorem_ipsum.php';?></p>
-						<H2 onclick="myFunction2('para_info2')">Infos perso</H2>
-						<p id="para_info2"><?php $para='info'; include 'lorem_ipsum.php';?></p>
-					</div>
-					<div id="div3">
-						<div id="comp">
-							<H2 onclick="myFunction('para_comp')">Competences</H2>
-							<p id="para_comp"><?php $para='comp'; include 'lorem_ipsum.php';?></p>
-						</div>
-						<div id="form">
-							<H2 onclick="myFunction('para_form')">Formations</H2>
-							<p id="para_form"><?php $para='form'; include 'lorem_ipsum.php';?></p>
-						</div>
-						<div id="exp">
-							<H2 onclick="myFunction('para_exp')">Experiences</H2>
-							<p id="para_exp"><?php $para='exp'; include 'lorem_ipsum.php';?></p>
-						</div>
-						<div id="hob">
-							<H2 onclick="myFunction('hob_img')">Hobbits</H2>
-							<img id="hob_img" src="resources/hobbits.jpeg" title="Hobbit" alt="Hobbit" />
-						</div>
-					</div>
-					<div id="div4">
-						<H2>Projets</H2>
-						<ul>
-							<li><a href="">EOE		</a></li>
-							<li><a href="">IVL-2	</a></li>
-							<li><a href="">ft_ls	</a></li>
-							<li><a href="">ft_printf</a></li>
-							<li><a href="">push_swap</a></li>
-						</ul>
-						<H2>Projets</H2>
-						<ul>
-							<li id="but">Click me!</li>
-							<ul id="projects">
-								<li>EOE</li>
-								<li>IVL-2</li>
-								<li>ft_ls</li>
-								<li>ft_printf</li>
-								<li>push_swap</li>
-							</ul>
-						</ul>
-					</div>
-				</div><!--portrait-->
-			</div><!--content-->
-		</div><!--bg-->
-<<<<<<< HEAD
-		<nav>
-		<a id="bot" href="#top">TO TOP</a>
-		</nav>
-=======
-		<img id="icon_DL_PDF" src="resources/download.png" />
-		<a href="includes/convertToPDF.php"><img id="icon_DL_PDF" src="resources/download.png" /></a>
-		<button id="DL_PDF">Télécharger en PDF</button>
-		<button id="elevator">Revenir en haut de la page</button>
-		<script>
-			$('#elevator').on('click', function() { elevator(); });
-			$('#icon_DL_PDF').click(function () { downloadPDF(); });
-			window.onscroll = scroll;
-
-			function scroll () {
-				document.body.style.backgroundPosition = "0px "+Math.round(window.pageYOffset/2)+"px";
-			}
-		</script>
->>>>>>> d6a7d12fe14bd2330db25dfacb2232dc5821954d
+		<div id="bg1">
+		</div>
+		<div id="bg2">
+		</div>
+		<div id="bg3">
+		</div>
 	</BODY>
 </HTML>
 
